1e4f4c6ee0c3cd16f0adff50d2de016265869ff6
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-08-13 15:00:33 +0900

    sketch

--- a.sh
+++ a.sh
@@ -3,17 +3,42 @@
 # - IN
 # curl -fsL 'https://kotlinlang.org/docs/reference/java-interop.html#mapped-types' -o test.html
 
+usage(){
 
-OUTPUT_FILE_PREFIX=Java-Kotlin
+cat <<EOS
+
+Usage:
+
+  CMD: $0 Kotlin-C
+
+EOS
+
+exit 0
+
+}
+
+OUTPUT_FILE_PREFIX=$1;shift;
 OUTPUT_FILE_SUFFIX=tsv
 
-table_tag_cnt=$(cat test.html | grep -cP '<table')
+if [ -z $OUTPUT_FILE_PREFIX ];then
+
+  usage
+
+fi
+
+table_tag_cnt=$(cat test.html | pup '.typo-table' | grep -cP '<table')
 
 seq 1 $table_tag_cnt | \
 
   while read n;do
 
-    xpath_table=$(cat test.html | pup ".typo-table:nth-of-type($n)"| teip -Gog '\s(id|class|href).*?(?=>)' -- sed 's/.//g'| grep -vP '<code>|</code>|<a>|</a>|<strong>|</strong>' | html2 | grep -P '/html/body/table/(thead|tbody)/tr/(th|td)')
+    if [[ $n -eq 1 ]];then
+      xpath_table=$(cat test.html | pup ".typo-table" | teip -Gog '\s(id|class|href).*?(?=>)' -- sed 's/.//g'| grep -vP '<code>|</code>|<a>|</a>|<strong>|</strong>' | html2 | grep -P '/html/body/table/(thead|tbody)/tr/(th|td)')
+
+    else
+      xpath_table=$(cat test.html | pup ".typo-table:nth-of-type($n)"| teip -Gog '\s(id|class|href).*?(?=>)' -- sed 's/.//g'| grep -vP '<code>|</code>|<a>|</a>|<strong>|</strong>' | html2 | grep -P '/html/body/table/(thead|tbody)/tr/(th|td)')
+
+    fi
 
     table_thead_tag_cnt=$(echo "$xpath_table" | grep -Pc 'thead')
 
